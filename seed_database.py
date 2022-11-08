589c1f6a7084032a15149373bcf81455a1278a1a
Mark Nakayama <mark.nakay@gmail.com>
2022-11-08 15:54:50 -0700

    added a seed_database.py to seed the PostgreSQL with dummy data for the project

--- seed_database.py
+++ seed_database.py
@@ -1,5 +1,7 @@
 # SEED_DATABASE.PY
 
+# On execiution will create a PostgreSQL database for use in program
+
 # SETUP
 ########################################################################
 
@@ -36,5 +38,46 @@ model.connect_to_db(server.app)
 # create all the tables for PostreSQL
 model.db.create_all()
 
-# EXECUTE
+# LOAD
 ########################################################################
+
+# Load movie data from JSON file
+with open("data/movies.json") as f:
+    movie_data = json.loads(f.read())
+
+# Create movies, store them in list for use
+# Create stock ratings
+movies_in_db = []
+for movie in movie_data:
+    title, overview, poster_path = (
+        movie["title"],
+        movie["overview"],
+        movie["poster_path"],
+    )
+    release_date = datetime.strptime(movie["release_date"], "%Y-%m-%d")
+
+    db_movie = crud.create_movie(title, overview, release_date, poster_path)
+    movies_in_db.append(db_movie)
+
+model.db.session.add_all(movies_in_db)
+model.db.session.commit()
+
+# Create Instance of users
+
+user_count = 10
+
+for n in range(user_count):
+    email = f"user{n}@test.com" # unique email generator
+    password = "test"
+
+    user = crud.create_user(email, password)
+    model.db.session.add(user)
+
+    for _ in range(10):
+        random_movie = choice(movies_in_db)
+        score = randint(1, 5)
+
+        rating = crud.create_rating(user, random_movie, score)
+        model.db.session.add(rating)
+
+model.db.session.commit()
